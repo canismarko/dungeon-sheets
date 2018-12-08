a1f12b10edfbf0f25a6dfd3c2bc1243f7bf979be
Jon Brouse <jbrouse19@gmail.com>
2018-12-08 03:57:57 +0000

    Added toll the dead spell

--- dungeonsheets/spells.py
+++ dungeonsheets/spells.py
@@ -4519,6 +4519,26 @@ class TimeStop(Spell):
     magic_school = "Transmutation"
     classes = ()
 
+class TollTheDead(Spell):
+    """You point at one creature you can see within range, and the sound
+    of a dolorous bell fills the air around it for a moment. The target
+    must succeed on a Wisdom saving throw or take 1d8 necrotic damage. If
+    the target is missing any of its hit points, it instead takes 1d12
+    necrotic damage.
+
+    The spell’s damage increases by one die when you reach 5th level (2d8
+    or 2d12), 11th level (3d8 or 3d12), and 17th level (4d8 or 4d12).
+
+    """
+    name = "Toll The Dead"
+    level = 0
+    casting_time = "1 action"
+    casting_range = "60 ft"
+    components = ('V',)
+    materials = ""
+    duration = "Instantaneous"
+    magic_school = "Necromancy"
+    classes = ()
 
 class TrueResurrection(Spell):
     """You touch a creature that has been dead for no longer than 200
