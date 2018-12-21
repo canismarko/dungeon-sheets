a69fe0df3e066341d5967527659b932de007c69c
Ben Cook <bcook@cfa.harvard.edu>
2018-12-20 22:14:42 -0500

    Added multiclass examples to Examples

--- examples/multiclass.py
+++ examples/multiclass.py
@@ -1,109 +1,78 @@
-dungeonsheets_version = "0.4.2"
+"""This file describes the heroic adventurer Multiclass.
 
-# Basic information
-name = 'Inara Serradon'
-# classes_levels: specify class and level or, if multiclass, specify as list
-# example:
-# classes_levels = ['wizard 3']  # 3rd level wizard
-# subclasses = [None]
-# also accepted, as long as only one class
-# classes_levels = 'fighter 2'
-# subclasses = None
-# multiclass example
-classes_levels = ['wizard 3', 'fighter 1', 'rogue 1']  # 5th level total
-subclasses = ['Necromancer', None, None]
-player_name = 'Mark'
-background = "Acolyte"
-race = "High-Elf"
-# level = 3  # no longer used
-alignment = "Chaotic good"
-xp = 2190
-hp_max = 16
+Modify this file as you level up and then re-generate the character
+sheet by running ``makesheets`` from the command line.
+
+"""
+
+dungeonsheets_version = "0.8.0"
+
+name = "Multiclass"
+classes_levels = ['fighter 4', 'wizard 2']
+player_name = "Ben"
+background = "Inheritor"
+race = "Tabaxi"
+alignment = "Lawful neutral"
+xp = 0
+hp_max = 10
 
 # Ability Scores
-strength = 10
-dexterity = 15
-constitution = 14
-intelligence = 16
-wisdom = 12
-charisma = 8
+strength = 15
+dexterity = 17
+constitution = 15
+intelligence = 15
+wisdom = 11
+charisma = 10
+
+# Select what skills you're proficient with
+skill_proficiencies = ('history', 'history', 'intimidation', 'survival', 'perception', 'stealth')
+
+# Named features / feats that aren't part of your classes,
+# race, or background.
+# Example:
+# features = ('Tavern Brawler',)  # take the optional Feat from PHB
+features = ()
+
+# If selecting among multiple feature options: ex Fighting Style
+# Example (Fighting Style):
+# feature_choices = ('Archery',)
+feature_choices = ()
 
 # Proficiencies and languages
-skill_proficiencies = [
-    'arcana',
-    'insight',
-    'investigation',
-    'perception',
-    'religion',
-]
-languages = "Common, Elvish, Draconic, Dwarvish, Goblin."
+languages = """[choose one], Common, [Choose One]"""
 
 # Inventory
-cp = 316
-sp = 283
-ep = 28
-gp = 125
+# TODO: Get yourself some money
+cp = 0
+sp = 0
+ep = 0
+gp = 0
 pp = 0
-weapons = ('greatsword', 'longsword')
-equipment = (
-    """Gallon of ale, red cloak, shortsword, longsword, jar of salt, vodka
-    (500mL), potion of vitality, wand of magic missiles (7/7),
-    component pouch, spellbook, backpack, bottle of ink, ink pen, 10
-    sheets of parchment, small knife, tome of historical lore, holy
-    symbol, prayer book, set of common clothes, pouch.""")
+
+# TODO: Put your equipped weapons and armor here
+weapons = [] # Example: ('shortsword', 'longsword')
+armor = "None" # Eg "light leather armor"
+shield = "None" # Eg "shield"
+
+equipment = """"""
+
+attacks_and_spellcasting = """"""
 
 # List of known spells
-spells = ('blindness deafness', 'burning hands', 'detect magic',
-          'false life', 'mage armor', 'mage hand', 'magic missile',
-          'prestidigitation', 'ray of frost', 'ray of sickness', 'shield',
-          'shocking grasp', 'sleep',)
+# Example: spells = ('magic missile', 'mage armor')
+spells = () # Todo: Learn some spells
+
 # Which spells have been prepared (not including cantrips)
-spells_prepared = ('blindness deafness', 'false life', 'mage armor',
-                   'ray of sickness', 'shield', 'sleep',)
+spells_prepared = ()
 
 # Backstory
-personality_traits = """I use polysyllabic words that convey the impression of
-erudition. Also, I’ve spent so long in the temple that I have little
-experience dealing with people on a casual basis."""
+# Describe your backstory here
+personality_traits = """"""
 
-ideals = """Knowledge. The path to power and self-improvement is through
-knowledge."""
+ideals = """"""
 
-bonds = """The tome I carry with me is the record of my life’s work so far,
-and no vault is secure enough to keep it safe."""
+bonds = """"""
 
-flaws = """I’ll do just about anything to uncover historical secrets that
-would add to my research."""
+flaws = """"""
 
-features_and_traits = (
-    """Spellcasting Ability: Intelligence is your spellcasting ability for
-    your spells. The saving throw DC to resist a spell you cast is
-    13. Your attack bonus when you make an attack with a spell is
-    +5. See the rulebook for rules on casting your spells.
-    
-    Arcane Recovery: You can regain some of your magical energy by
-    studying your spellbook. Once per day during a short rest, you can
-    choose to recover expended spell slots with a combined level equal
-    to or less than half your wizard level (rounded up).
-    
-    Darkvision: You see in dim light within a 60-foot radius of you as
-    if it were bright light, and in darkness in that radius as if it
-    were dim light. You can’t discern color in darkness, only shades
-    of gray.
-    
-    Fey Ancestry: You have advantage on saving throws against being
-    charmed, and magic can’t put you to sleep.
-    
-    Trance: Elves don’t need to sleep. They meditate deeply, remaining
-    semiconscious, for 4 hours a day and gain the same benefit a human
-    does from 8 hours of sleep.
-    
-    Shelter of the Faithful: As a servant of Oghma, you command the
-    respect of those who share your faith, and you can perform the
-    rites of Oghma. You and your companions can expect to receive free
-    healing and care at a temple, shrine, or other established
-    presence of Oghma’s faith. Those who share your religion will
-    support you (and only you) at a modest lifestyle. You also have
-    ties to the temple of Oghma in Neverwinter, where you have a
-    residence. When you are in Neverwinter, you can call upon the
-    priests there for assistance that won’t endanger them.""")
+features_and_traits = """"""
