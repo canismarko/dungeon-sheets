d723a047c63cb759e881bb797aef0af00a86b707
Ben Cook <bcook@cfa.harvard.edu>
2019-01-02 15:47:52 -0500

    updated tests

--- tests/test_make_sheets.py
+++ tests/test_make_sheets.py
@@ -4,13 +4,13 @@ import os
 from dungeonsheets import make_sheets, character
 
 EG_DIR = os.path.abspath(os.path.join(os.path.split(__file__)[0], '../examples/'))
-CHARFILE = os.path.join(EG_DIR, 'rogue.py')
+CHARFILE = os.path.join(EG_DIR, 'rogue1.py')
 
 class CharacterFileTestCase(unittest.TestCase):
     def test_load_character_file(self):
         charfile = CHARFILE
         result = make_sheets.load_character_file(charfile)
-        self.assertEqual(result['strength'], 8)
+        self.assertEqual(result['strength'], 10)
 
 
 class PdfOutputTeestCase(unittest.TestCase):
