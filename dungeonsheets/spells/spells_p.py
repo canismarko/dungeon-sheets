51c5e8132231411f647f65b15e0407731f374ee0
Mark Wolfman <canismarko@gmail.com>
2020-07-09 14:04:54 -0500

    Remove illegal unicode character (\227) from spells_p.py descriptions.

--- dungeonsheets/spells/spells_p.py
+++ dungeonsheets/spells/spells_p.py
@@ -45,39 +45,42 @@ class Passwall(Spell):
 
 
 class PhantasmalForce(Spell):
-    """You craft an illusion that takes root in the mind of a creature that you can see
-     within range.
-    The target must make an Intelligence saving throw. On a failed
-    save, you create a phantasmal object, creature, or other visible phenomenon of
-    your choice that is no larger than a 10-foot cube and that is perceivable only
-    to the target for the duration. This spell has no effect on undead or
-    constructs.
-
+    """You craft an illusion that takes root in the mind of a creature
+    that you can see within range. The target must make an
+    Intelligence saving throw. On a failed save, you create a
+    phantasmal object, creature, or other visible phenomenon of your
+    choice that is no larger than a 10-foot cube and that is
+    perceivable only to the target for the duration. This spell has no
+    effect on undead or constructs.
+    
     The phantasm includes sound, temperature, and other stimuli, also
     evident only to the creature.
-
-    The target can use its action to examine the
-    phantasm with an Intelligence (Investigation) check against your spell save DC.
-    If the check succeeds, the target realizes that the phantasm is an illusion, and
-     the spell ends.
-
+    
+    The target can use its action to examine the phantasm with an
+    Intelligence (Investigation) check against your spell save DC. If
+    the check succeeds, the target realizes that the phantasm is an
+    illusion, and the spell ends.
+    
     While a target is affected by the spell, the target treats the
-     phantasm as if it were real. The target rationalizes any illogical outcomes
-    from interacting with the phantasm. For example, a target attempting to walk
-    across a phantasmal bridge that spans a chasm falls once it steps onto the
-    bridge. If the target survives the fall, it still believes that the bridge
-    exists and comes up with some other explanation for its fallit was pushed, it
-    slipped, or a strong wind might have knocked it off.
-
-    An affected target is so
-    convinced of the phantasm's reality that it can even take damage from the
-    illusion. A phantasm created to appear as a creature can attack the target.
-    Similarly, a phantasm created to appear as fire, a pool of acid, or lava can
-    burn the target. Each round on your turn, the phantasm can deal 1d6 psychic
-    damage to the target if it is in the phantasm's area or within 5 feet of the
-    phantasm, provided that the illusion is of a creature or hazard that could
-    logically deal damage, such as by attacking. The target perceives the damage as
-    a type appropriate to the illusion.
+    phantasm as if it were real. The target rationalizes any illogical
+    outcomes from interacting with the phantasm. For example, a target
+    attempting to walk across a phantasmal bridge that spans a chasm
+    falls once it steps onto the bridge. If the target survives the
+    fall, it still believes that the bridge exists and comes up with
+    some other explanation for its fall - it was pushed, it slipped,
+    or a strong wind might have knocked it off.
+    
+    An affected target is so convinced of the phantasm's reality that
+    it can even take damage from the illusion. A phantasm created to
+    appear as a creature can attack the target. Similarly, a phantasm
+    created to appear as fire, a pool of acid, or lava can burn the
+    target. Each round on your turn, the phantasm can deal 1d6 psychic
+    damage to the target if it is in the phantasm's area or within 5
+    feet of the phantasm, provided that the illusion is of a creature
+    or hazard that could logically deal damage, such as by
+    attacking. The target perceives the damage as a type appropriate
+    to the illusion.
+    
     """
     name = "Phantasmal Force"
     level = 2
@@ -593,75 +596,80 @@ class PrismaticSpray(Spell):
 
 
 class PrismaticWall(Spell):
-    """A shimmering, multicolored plane of light forms a vertical opaque wallup to 90
-    feet long, 30 feet high, and 1 inch thickcentered on a point you can seewithin
-    range. Alternatively, you can shape the wall into a sphere up to 30 feet in
-    diameter centered on a point you choose within range. The wall remains in place
-    for the duration. If you position the wall so that it passes through a space
-    occupied by a creature, the spell fails, and your action and the spell slot are
-    wasted.
+    """A shimmering, multicolored plane of light forms a vertical opaque
+    wall-up to 90 feet long, 30 feet high, and 1 inch thick-centered
+    on a point you can see within range. Alternatively, you can shape
+    the wall into a sphere up to 30 feet in diameter centered on a
+    point you choose within range. The wall remains in place for the
+    duration. If you position the wall so that it passes through a
+    space occupied by a creature, the spell fails, and your action and
+    the spell slot are wasted.
 
-    The wall sheds bright light out to a range of 100 feet and dim light
-    for an additional 100 feet. You and creatures you designate at the time you cast
-     the spell can pass through and remain near the wall without harm. If another
-    creature that can see the wall moves to within 20 feet of it or starts its turn
-    there, the creature must succeed on a Constitution saving throw or become
+    The wall sheds bright light out to a range of 100 feet and dim
+    light for an additional 100 feet. You and creatures you designate
+    at the time you cast the spell can pass through and remain near
+    the wall without harm. If another creature that can see the wall
+    moves to within 20 feet of it or starts its turn there, the
+    creature must succeed on a Constitution saving throw or become
     blinded for 1 minute.
 
     The wall consists of seven layers, each with a different
-    color. When a creature attempts to reach into or pass through the wall, it does
-    so one layer at a time through all the wall's layers. As it passes or reaches
-    through each layer, the creature must make a Dexterity saving throw or be
-    affected by that layer's properties as described below.
+    color. When a creature attempts to reach into or pass through the
+    wall, it does so one layer at a time through all the wall's
+    layers. As it passes or reaches through each layer, the creature
+    must make a Dexterity saving throw or be affected by that layer's
+    properties as described below.
 
-    The wall can be
-    destroyed, also one layer at a time, in order from red to violet, by means
-    specific to each layer. Once a layer is destroyed, it remains so for the
-    duration of the spell. A rod of cancellation destroys a prismatic wall, but an
-    antimagic field has no effect on it.
+    The wall can be destroyed, also one layer at a time, in order from
+    red to violet, by means specific to each layer. Once a layer is
+    destroyed, it remains so for the duration of the spell. A rod of
+    cancellation destroys a prismatic wall, but an antimagic field has
+    no effect on it.
 
-    1. Red. The creature takes 10d6 fire
-    damage on a failed save, or half as much damage on a successful one. While this
-    layer is in place, nonmagical ranged attacks can't pass through the wall. The
+    1. Red. The creature takes 10d6 fire damage on a failed save, or
+    half as much damage on a successful one. While this layer is in
+    place, nonmagical ranged attacks can't pass through the wall. The
     layer can be destroyed by dealing at least 25 cold damage to it.
 
-    2. Orange. The
-     creature takes 10d6 acid damage on a failed save, or half as much damage on a
-    successful one. While this layer is in place, magical ranged attacks can't pass
-    through the wall. The layer is destroyed by a strong wind.
+    2. Orange. The creature takes 10d6 acid damage on a failed save,
+    or half as much damage on a successful one. While this layer is in
+    place, magical ranged attacks can't pass through the wall. The
+    layer is destroyed by a strong wind.
 
-    3. Yellow. The
-    creature takes 10d6 lightning damage on a failed save, or half as much damage on
-     a successful one. This layer can be destroyed by dealing at least 60 force
-    damage to it.
+    3. Yellow. The creature takes 10d6 lightning damage on a failed
+    save, or half as much damage on a successful one. This layer can
+    be destroyed by dealing at least 60 force damage to it.
 
     4. Green. The creature takes 10d6 poison damage on a failed save,
-     or half as much damage on a successful one. A passwall spell, or another spell
-    of equal or greater level that can open a portal on a solid surface, destroys
-    this layer.
+    or half as much damage on a successful one. A passwall spell, or
+    another spell of equal or greater level that can open a portal on
+    a solid surface, destroys this layer.
 
     5. Blue. The creature takes 10d6 cold damage on a failed save, or
-    half as much damage on a successful one. This layer can be destroyed by dealing
-    at least 25 fire damage to it.
+    half as much damage on a successful one. This layer can be
+    destroyed by dealing at least 25 fire damage to it.
 
-    6. Indigo. On a failed save, the creature is
-    restrained. It must then make a Constitution saving throw at the end of each of
-    its turns. If it successfully saves three times, the spell ends. If it fails its
-     save three times, it permanently turns to stone and is subjected to the
-    petrified condition. The successes and failures don't need to be consecutive;
-    keep track of both until the creature collects three of a kind. While this layer
-     is in place, spells can't be cast through the wall. The layer is destroyed by
-    bright light shed by a daylight spell or a similar spell of equal or higher
-    level.
+    6. Indigo. On a failed save, the creature is restrained. It must
+    then make a Constitution saving throw at the end of each of its
+    turns. If it successfully saves three times, the spell ends. If it
+    fails its save three times, it permanently turns to stone and is
+    subjected to the petrified condition. The successes and failures
+    don't need to be consecutive; keep track of both until the
+    creature collects three of a kind. While this layer is in place,
+    spells can't be cast through the wall. The layer is destroyed by
+    bright light shed by a daylight spell or a similar spell of equal
+    or higher level.
 
-    7. Violet. On a failed save, the creature is blinded. It must then make
-    a Wisdom saving throw at the start of your next turn. A successful save ends the
-     blindness. If it fails that save, the creature is transported to another plane
-    of the DM's choosing and is no longer blinded. (Typically, a creature that is on
-     a plane that isn't its home plane is banished home, while other creatures are
-    usually cast into the Astral or Ethereal planes.) This layer is destroyed by a
-    dispel magic spell or similar spell of equal or higher level that can end spells
-     and magical effects.
+    7. Violet. On a failed save, the creature is blinded. It must then
+    make a Wisdom saving throw at the start of your next turn. A
+    successful save ends the blindness. If it fails that save, the
+    creature is transported to another plane of the DM's choosing and
+    is no longer blinded. (Typically, a creature that is on a plane
+    that isn't its home plane is banished home, while other creatures
+    are usually cast into the Astral or Ethereal planes.) This layer
+    is destroyed by a dispel magic spell or similar spell of equal or
+    higher level that can end spells and magical effects.
+
     """
     name = "Prismatic Wall"
     level = 9
