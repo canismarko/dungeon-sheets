1d67463566d1337905b070b435b004e6dc654c6a
Mark Wolfman <canismarko@gmail.com>
2021-09-17 20:08:44 -0500

    Added random table for curses.

--- dungeonsheets/random_tables.py
+++ dungeonsheets/random_tables.py
@@ -1056,19 +1056,19 @@ class MagicItemTableG(RandomTable):
     +-------+-------------------------------------------+
     | 12–14 | Figurine of wondrous power (roll d8)      |
     +-------+-------------------------------------------+
-    |     - | 1: Bronze griffon                         |
+    |    -- | 1: Bronze griffon                         |
     +-------+-------------------------------------------+
-    |     - | 2: Ebony fly                              |
+    |    -- | 2: Ebony fly                              |
     +-------+-------------------------------------------+
-    |     - | 3: Golden lions                           |
+    |    -- | 3: Golden lions                           |
     +-------+-------------------------------------------+
-    |     - | 4: Ivory goats                            |
+    |    -- | 4: Ivory goats                            |
     +-------+-------------------------------------------+
-    |     - | 5: Marble elephant                        |
+    |    -- | 5: Marble elephant                        |
     +-------+-------------------------------------------+
-    |     - | 6-7: Onyx dog                             |
+    |    -- | 6-7: Onyx dog                             |
     +-------+-------------------------------------------+
-    |     - | 8: Serpentine owl                         |
+    |    -- | 8: Serpentine owl                         |
     +-------+-------------------------------------------+
     |    15 | Adamantine armor (breastplate)            |
     +-------+-------------------------------------------+
@@ -1456,19 +1456,19 @@ class MagicItemTableI(RandomTable):
     +-------+---------------------------------------+
     |    76 | Magic armor (roll dl2)                |
     +-------+---------------------------------------+
-    |     - | 1-2: Armor, +2 half plate             |
+    |    -- | 1-2: Armor, +2 half plate             |
     +-------+---------------------------------------+
-    |     - | 3-4: Armor, +2 plate                  |
+    |    -- | 3-4: Armor, +2 plate                  |
     +-------+---------------------------------------+
-    |     - | 5-6: Armor, +3 studded leather        |
+    |    -- | 5-6: Armor, +3 studded leather        |
     +-------+---------------------------------------+
-    |     - | 7-8: Armor, +3 breastplate            |
+    |    -- | 7-8: Armor, +3 breastplate            |
     +-------+---------------------------------------+
-    |     - | 9-10: Armor, +3 splint                |
+    |    -- | 9-10: Armor, +3 splint                |
     +-------+---------------------------------------+
-    |     - | 11: Armor, +3 half plate              |
+    |    -- | 11: Armor, +3 half plate              |
     +-------+---------------------------------------+
-    |     - | 12: Armor, +3 plate                   |
+    |    -- | 12: Armor, +3 plate                   |
     +-------+---------------------------------------+
     |    77 | Apparatus of Kwalish                  |
     +-------+---------------------------------------+
@@ -1533,3 +1533,111 @@ class Treasure(RandomTable):
                  MagicItemTableA, MagicItemTableB, MagicItemTableC,
                  MagicItemTableD, MagicItemTableE, MagicItemTableF,
                  MagicItemTableG, MagicItemTableH, MagicItemTableI]
+
+
+class Curses(RandomTable):
+    """
+
+    - 1: The character cannot turn right until the curse is lifted.
+    - 2: Characters feet always sink at least 1 inch into any surface they walk on (the at least accounts for walking on water, as in if they try to walk on water they sink normally)
+    - 3: All [food] type becomes tasteless (meat, vegetables, fruit)
+    - 4: When the character fires a ranged weapon, the ammunition always breaks on impact (no effect on damage)
+    - 5: Until the curse is lifted, when the character falls to 0hp, roll a D100. If you roll equal to or below the CR of the creature that cursed you, you instantly die.
+    - 6: A player must close every door they walk through, even if there are people behind them.
+    - 7: A player's weapon becomes lodged inside the body of their enemy after any stab attack, a strength check (DC 15) is needed to free the weapon.
+    - 8: A player's weapons become twice as heavy, requiring two actions to strike once, until the curse is lifted.
+    - 9: A player is stalked by an imp, who simply follows him, saying nothing, always staring. No one else can see the imp.
+    - 10: The player's backpack is enchanted, to always give the player an item they needed in the past, but never what they will need in the future or present.
+    - 11: The next item the cursed player grabs is bound to them forever, they can never get rid of it.
+    - 12: Everytime the cursed character kills someone stealthily, the slain thing lets out an incredibly loud scream that can be heard from 500 ft away, even if it wouldn't be possible for the dead thing to scream.
+    - 13: Character takes on the appearance and smell of being undead, but isn't.
+    - 14: Characters must only answer questions with lies, unless they are asked about the reason for their behaviour (ex: "are you cursed?" "Are you lying on purpose?") In which case they must respond in the affirmative.
+    - 15: Characters must agree to every suggestion or request made within 30 feet of them. Curse is broken after a week.
+    - 16: The cursed character takes 1 damage whenever a creature within 30 feet of them takes any damage.
+    - 17: Character cannot willingly kill/spare the life of any living creature (choose depending on character personality).
+    - 18: Character becomes incapable of visually perceiving living creatures.
+    - 19: Characters low-light vision and high-light vision switch (i.e. sunlight is effectively dark, but can see areas in shadow as if they were brightly lit).
+    - 20: Roll a d100. After the amount of dies shown on dice, the character explodes for (as per a 5th level Fireball) the next time they take a long rest, then is immediately put under the effects of a Reincarnation spell. The cycle continues until a Wish spell dispels it.
+    - 21: Character is struck with blindness, but can accurately identify objects by taste through the air up to 60 feet away.
+    - 22: Butt switches place with face. Switches every time either orifice expels any substance.
+    - 23: Your CHA stat becomes your CHAR stat, determining your effectiveness at cooking up a mean barbeque. Reflavour spells and skill checks accordingly.
+    - 24: When the target of the curse next goes to sleep, they dream of a burning lake. The dreams progress, becoming nightmares over time. The target instinctively becomes aware of the direction of the lake, and must save vs Wis or spend that day trying to reach the lake. The target must save every day to prevent the condition progressing, taking a penalty to mental rolls for every stage it advances. To completely recover, the target must make 3 saves in a row, if they fail a save it regresses to its initial condition, and if they fail 3 times in a row the target becomes maddened until they reach the lake. Upon reaching the lake they will see it is not engulfed in flames, and will take d6 Psychic damage for the number of days they have been affected by the urges.
+    - 25: The first ritual performed after being cursed succeeds instantly, but when they next sleep the target must save vs. Con. If they fail, their skin dries and their body catches alight, taking d6 damage per turn. The fire can be put out by magical or mundane conventions.
+    - 26: The cursed begins aging at 5 years an hour. When they reach 100 years, they die, and an infant crawls from their body’s clothing. It continues to age at the same rate until it reaches 20. Same character, same memories.
+    - 27: As the curse is activated, the target's hands detach from their wrists and scuttle away, and new hands grow in their place. For the rest of the day, every time they cast a spell, the same thing happens. The hands remain animate until destroyed, and will do their best to make terrible mischief.
+    - 28: A thunderous voice narrates everything the cursed does, says, or thinks for the next d4 hours.
+    - 29: For the next d4 days, every time the cursed attempts to speak, including to cast a spell, they must Save vs Int or instead deliver a lengthy and discursive monologue on: 1: bean cultivation; 2: the daily schedule of an emperor who died thousands of years ago; 3: the spiritual beliefs of spiders; 4: the life cycle of the cherub; 5: the various manias, phobias, or perversions of the nearest, most powerful monarch; 6: the correct method of preparing, storing, and administering a heretofore unknown and spectacularly deadly poison; 7: the best tourist destinations in the nearest village; 8: famous fish poets; 9: the dangers of breathing; 10: the magical properties of cheese; On a repeated roll, the target must continue their lecture from where they left off before.
+    - 30: Until the curse is lifted the character constantly sniffs and has a runny nose. Disadvantage on stealth, persuasion and deception checks.
+    - 31: Character can not control the volume at which he speaks. Player rolls a D6 every time their character speaks, even rolls are spoken in a whisper, odd rolls are shouting.
+    - 32: Animals and children are always aware of your presence and are able to locate you without difficulty.
+    - 33: "Curse of Popularity" - In populated area with non-hostile NPCs, everyone knows who you are and will not leave you alone. Roll a charisma check/save (DC varies). If failed, you are viewed in an unfavorable light. If passed, you are viewed in a favorable one.
+    - 34: Everytime a player deals damage the same amount is reflected back to a random party member.
+    - 35: All food and drink consumed immediately tastes of rotten flesh a successful fortitude save of DC 15 can overcome this taste.
+    - 36: Whenever the PC comes into a hallway/corridor they are compelled to Sprint at full speed to the end. Will save to resist at DMs discretion.
+    - 37: The PC must only speak in rhyme.
+    - 38: The PC gains a new fear based on popular vote of the party until dispelled.
+    - 39: All the player's equipment glows brightly for 24 hours. All of it.
+    - 40: The player becomes magnetic.
+    - 41: It is always raining in a 5ft cube around the player. The intensity randomly varies from a drizzle to a downpour and can exist even underwater or indoors.
+    - 42: The character finds themselves unable to open any containers or doors which require a twisting motion.
+    - 43: The character perceives traps everywhere where none exist.
+    - 44: The cursed becomes lactose intolerant. Consuming any dairy leads to 1d4 hour(s) of insufferable gas & diarrhea.
+    - 45: The player must compulsively juggle items any time the player has two or more of an object in easy reach. DC 10+the number of items being juggled Acrobatics check, or an item gets dropped, with appropriate consequences.
+    - 46: Boots squeak loudly with each step.
+    - 47: Effects of alcohol are heavily amplified, so that even drinking one drop of a fairly weak alcoholic beverage will make the PC drunk. Drinking a full glass of a strong alcoholic beverage could potentially cause death.
+    - 48: All of the PC's armor and clothing teleported off their body and always floats just out of reach. Any attempt to put on other clothing or armor produces the same effect.
+    - 49: Character's known languages are randomly determined after a long rest. Roll 1d8 per standard language known & 1d8 per exotic language known. You decide whether to exclude common from these rolls or not.
+    - 50: After a long rest a random amount of GP the character is carrying is randomly changed to an amount of either Electrum, Silver or Copper pieces of the same worth, increasing number of coins. eg. (1d20 Amount, Roll 1d6 to determine type) Won't take long for pockets to become overflowing if character doesn't spend loose change.
+    - 51: Any divination spells where the caster or target is within a certain range of a character are retargeted to that character.
+    - 52: The cursed begins to weep tears of blood uncontrollably, reducing their hit point maximum by 1 for every hour the curse remains active. The cursed dies if this effect reduces its hit point maximum to 0.
+    - 53: The cursed is compelled to repeat the last word of each sentence they say 3 times, each time speaking a little bit softer than the last. If the curse remains active for more than 24 hours, the cursed is compelled to dramatically flick their hands open and closed with each echo.
+    - 54: Cursed characters are hated by all cats until cured. Every cat will hiss and attempt to swipe and bite the character. Irregardless of the cat is successful or not the cat will run away and hide. If the cat is successful in the attack any wounds caused will not heal (even with healing spells and potions) and will continuously weep foul smelling pus.
+    - 55: The character cannot be convinced by any means that magic exists. They rationalize magical events away by using insane, impossible logic.
+    - 56: The character believes themselves to have swapped bodies with the nearest person. Nothing has happened.
+    - 57: One of the character's limbs no longer has any bones. It flops around uselessly until the bones have successfully regrown in 1d4 days.
+    - 58: The character is unable to sleep when others are sleeping in a 60' radius.
+    - 59: Once the character has fallen asleep they cannot be awoken by any non-magical means until 8 hours have passed.
+    - 60: The character must consume 1d4+1 times the amount of food and drink a normal person does do sustain themselves. They experience terrible thirst and hunger pains. Treat as exhaustion if they do not actively maintain this regimen.
+    - 61: The character cannot see anyone within 10 feet of them.
+    - 62: The character finds a wooden spoon in their bag. Every time they retrieve an item they find another wooden spoon. Every time they investigate an area they find another wooden spoon. Every time the search a body they find another wooden spoon. If they intentionally attempt to locate, retrieve, or use a spoon the task is impossible.
+    - 63: Incapable of ignoring direct orders given to their person.
+    - 64: When splashed with cold water transforms the character into the opposite gender. Warm water temporarily reverses the transition.
+    - 65: Must make one significant lie per day.
+    - 66: Automatically fails all swimming checks; it's as if the character weights 10 times their normal weight while in water.
+    - 67: Turned into a lycanthrope... with the form of a rabbit.
+    - 68: All creatures of a specific species are invisible to the character.
+    - 69: A perpetually magical darkness surrounds the character for 25 feet. It is transmittable by touch.
+    - 70: When killed for the first time each day, the wounds heal and they instead stabilize. If they are not killed once a day, they are permanently slain.
+    - 71: Makes an unarmed attack against themselves whenever they say 'what'.
+    - 72: Characters ears and eyes switch place. PC cannot look straight ahead. This lasts until the next full day ends.
+    - 73: PC summons a little foot tall naked man with a hat that goofs around and makes as much noise as possible. Everyone can hear him and see his impact in the area, but cannot see him. He has no name and will not speak back. He just speaks in sounds and screams.
+    - 74: PC thinks their eyes have the same effect as a beholder and use them as much as possible in combat.
+    - 75: PCs teeth are as weak as glass.
+    - 76: PCs weapon changes to the next material they touch.
+    - 77: PC is followed by all bugs within 20 yards. (Bonus points if there are ant hills around).
+    - 78: PC grows a mouth in their chest. You can feed the mouth but you do not know what it will do. Overtime the mouth will grow if unfed.
+    - 79: PCs money all goes down a material (gold turns to silver) until curse is lifted. Copper turns into wooden toy coins that children would play with.
+    - 80: All plants the PC touches turn to dust for the next week. (Bonus points if a druid gets this)
+    - 81: All potions being held by PC give a delusion effect (example: PC thinks they are invisible but are not.)
+    - 82: All damage given to the PC for the next 12 minutes are irreversible.
+    - 83: The next person the PC touches switches all items.
+    - 84: PC's armor or clothing (whichever applicable) is made entirely out of shards of glass magically held together.
+    - 85: Character must kill one humanoid per week or die themselves.
+    - 86: Character must read one book per week or die, it must be a book they have not read before.
+    - 87: Every dawn, gravity reverses for the character for one hour.
+    - 88: Whenever the character physically harms another sentient being the character must apologize.
+    - 89: Whenever the character is on a sea vehicle of any kind the character vomits anything he/she eats and cannot sleep.
+    - 90: The PCs hand's tense up, and are stuck in a fist until the curse is removed.
+    - 91: The character's left and right hands, and/or left and right feet switch sides until the curse is lifted.
+    - 92: This curse hardens all food this character tries to eat like stone, unless they have the correct eating utensil to eat it. (ie They need a spoon to eat soup, a fork to eat pie, a knife to cut meat, etc).
+    - 93: The player believes that their mentor/parent has just died.
+    - 94: The player receives at least one false vision from their deity a day.
+    - 95: The cursed player can no longer fail the expectations of those they come across. (For example, if an NPC learns the player's name and they believe their name to be elfish, they will become an elf. If they expect that he is a weakling due to rumors they have heard of him losing an important fight, the player looses some of his strength. If they thought he would be taller, the character would become taller, etc...)
+    - 96: The player is cursed to look down at the ground; they can no longer make eye contact with others, unless they are able to look down on them...
+    - 97: Until this player's curse is lifted, as long as they wear shoes/boots, they will feel as if their feet are walking on burning hot coals.
+    - 98: This player has been cursed to be afraid of the sun.
+    - 99: This curse makes the player compelled to hug all characters they come across, even if it would be inappropriate or awkward.
+    - 100: This curse makes the character forcibly say gibberish every time they cast a spell.
+    
+    """
+    name = "Curses"
