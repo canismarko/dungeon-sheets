b53d443514a2d4bc29526fff8bda2f1517232235
Marian Minar <majamin@gmail.com>
2021-10-10 12:39:10 -0700

    add Channel Divinity features to Oath of Vengeance Paladin

--- dungeonsheets/classes/paladin.py
+++ dungeonsheets/classes/paladin.py
@@ -155,6 +155,7 @@ class OathOfVengeance(PaladinOath):
         17: [spells.HoldMonster, spells.Scrying],
     }
     features_by_level = defaultdict(list)
+    features_by_level[3] = [features.AbjureEnemy, features.VowOfEnmity]
     features_by_level[7] = [features.RelentlessAvenger]
     features_by_level[15] = [features.SoulOfVengeance]
     features_by_level[20] = [features.AvengingAngel]
--- dungeonsheets/features/paladin.py
+++ dungeonsheets/features/paladin.py
@@ -263,6 +263,33 @@ class HolyNimbus(Feature):
 
 # Oath of Vengeance
 
+class AbjureEnemy(Feature):
+    """As an action, you present your holy symbol and speak a prayer of denunciation,
+    using your Channel Divinity. Choose one creature within 60 feet of you that
+    you can see. That creature must make a Wisdom saving throw, unless it is immune
+    to being frightened. Fiends and undead have disadvantage on this saving throw.
+
+    On a failed save, the creature is frightened for 1 minute or until it takes
+    any damage. While frightened, the creature's speed is 0, and it can't benefit
+    from any bonus to its speed.
+
+    On a successful save, the creature's speed is halved for 1 minute or until the
+    creature takes any damage.
+    """
+
+    name = "Channel Divinity: Abjure Enemy"
+    source = "Paladin (Oath of Vengeance)"
+
+class VowOfEnmity(Feature):
+    """ As a bonus action, you can utter a vow of enmity against a creature you can
+    see within 10 feet of you, using your Channel Divinity. You gain advantage on
+    attack rolls against the creature for 1 minute or until it drops to 0 hit points
+    or falls unconscious.
+    """
+
+    name = "Channel Divinity: Vow of Enmity"
+    source = "Paladin (Oath of Vengeance)"
+
 class RelentlessAvenger(Feature):
     """By 7th level, your supernatural focus helps you close off a foe's retreat.
     When you hit a creature with an opportunity attack, you can move up to half
