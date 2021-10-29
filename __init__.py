9736f29f017bea020f70f5f843e6985585f6dd93
N3SSUNO <93273173+plsaddbloodhunter@users.noreply.github.com>
2021-10-29 16:45:04 +0200

    Blood Hunter Class
    I hope this is the right way to send you the script i coded. It works locally i renamed the feature files for upload purposes.

---
+++ __init__.py
@@ -0,0 +1,53 @@
+__all__ = (
+    "CharClass",
+    "Artificer",
+    "Barbarian",
+    "Bard",
+    "Cleric",
+    "Druid",
+    "Fighter",
+    "Monk",
+    "Paladin",
+    "Ranger",
+    "Rogue",
+    "Sorceror",
+    "Warlock",
+    "Wizard",
+    "RevisedRanger",
+    "BloodHunter",
+    "available_classes",
+)
+
+from dungeonsheets.classes.artificer import Artificer
+from dungeonsheets.classes.barbarian import Barbarian
+from dungeonsheets.classes.bard import Bard
+from dungeonsheets.classes.classes import CharClass
+from dungeonsheets.classes.cleric import Cleric
+from dungeonsheets.classes.druid import Druid
+from dungeonsheets.classes.fighter import Fighter
+from dungeonsheets.classes.bloodhunter import BloodHunter
+from dungeonsheets.classes.monk import Monk
+from dungeonsheets.classes.paladin import Paladin
+from dungeonsheets.classes.ranger import Ranger, RevisedRanger
+from dungeonsheets.classes.rogue import Rogue
+from dungeonsheets.classes.sorceror import Sorceror
+from dungeonsheets.classes.warlock import Warlock
+from dungeonsheets.classes.wizard import Wizard
+
+available_classes = [
+    Artificer,
+    Barbarian,
+    Bard,
+    Cleric,
+    Druid,
+    Fighter,
+    Monk,
+    Paladin,
+    Ranger,
+    Rogue,
+    Sorceror,
+    Warlock,
+    Wizard,
+    RevisedRanger,
+    BloodHunter,
+]
