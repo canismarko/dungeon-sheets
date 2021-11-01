36273a379ae178b3189eef08e1b9e27b5ebcfc2b
N3SSUNO <93273173+plsaddbloodhunter@users.noreply.github.com>
2021-11-01 15:44:51 +0100

    Add files via upload

---
+++ dungeonsheets/features/__init__Feature.py
@@ -0,0 +1,24 @@
+from dungeonsheets.features.features import Feature, create_feature, all_features
+
+from dungeonsheets.features.artificer import *
+from dungeonsheets.features.backgrounds import *
+from dungeonsheets.features.barbarian import *
+from dungeonsheets.features.bard import *
+from dungeonsheets.features.cleric import *
+from dungeonsheets.features.druid import *
+from dungeonsheets.features.feats import *
+from dungeonsheets.features.fighter import *
+from dungeonsheets.features.bloodhunter import *
+from dungeonsheets.features.monk import *
+from dungeonsheets.features.paladin import *
+from dungeonsheets.features.races import *
+from dungeonsheets.features.ranger import *
+from dungeonsheets.features.rogue import *
+from dungeonsheets.features.sorceror import *
+from dungeonsheets.features.warlock import *
+from dungeonsheets.features.wizard import *
+
+from dungeonsheets.content_registry import default_content_registry
+
+
+default_content_registry.add_module(__name__)
