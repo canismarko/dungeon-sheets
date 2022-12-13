4575cb7c9caa1776a2f56a76e9300c488da337fc
Mark Wolfman <canismarko@gmail.com>
2022-12-13 11:24:04 -0600

    Removed python 3.6 as it is end-of-life and added python 3.10 -- 3.11.

--- setup.py
+++ setup.py
@@ -47,15 +47,17 @@ setup(
             "create-character = dungeonsheets.create_character:main",
         ]
     },
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Games/Entertainment :: Role-Playing",
     ],
 )
