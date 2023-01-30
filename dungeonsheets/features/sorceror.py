4af4bdbb2651c5d51dfbcd514f66723e5bf96a21
PJ Beers <pjbrs@floorenpj.nl>
2023-01-30 22:42:57 +0100

    Features: Sorceror: Fix "Favored by the Gods"

--- dungeonsheets/features/sorceror.py
+++ dungeonsheets/features/sorceror.py
@@ -360,7 +360,9 @@ class DivineMagic(Feature):
 class FavoredByTheGods(Feature):
     """Starting at 1st level, divine power guards your destiny. If you fail a
     saving throw or miss with an attack roll, you can roll 2d4 and add it to
-    the total, possibly changing
+    the total, possibly changing the outcome.
+    Once you use this feature, you can’t use it again until you finish a short
+    or long rest.
 
     """
 
